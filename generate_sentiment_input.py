69e3807e980093137f3bb4a920c32ce134d72cd6
Atmananda Persaud <apersaud@usc.edu>
2015-01-29 00:35:55 -0800

    updated scripts to generate spam and sentiment input files

--- generate_sentiment_input.py
+++ generate_sentiment_input.py
@@ -1,17 +1,17 @@
 import os
 import string
 
-filenames = os.listdir('data/SPAM_training')
-outfile = open('spam_training.txt', 'w')
+filenames = os.listdir('data/SENTIMENT_training')
+outfile = open('sentiment_training.txt', 'w')
 
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 
 for filename in filenames:
-    if filename[0] == 'H':
-        outfile.write('HAM ')
-    elif filename[0] == 'S':
-        outfile.write('SPAM ')
-    infile = open('data/SPAM_training/' + filename, errors='ignore')
+    if filename[0] == 'P':
+        outfile.write('POS ')
+    elif filename[0] == 'N':
+        outfile.write('NEG ')
+    infile = open('data/SENTIMENT_training/' + filename, errors='ignore')
     infiletext = infile.read()
     infiletext = infiletext.replace('\n', ' ')
     infiletext = infiletext.translate(remove_punctuation_map)
