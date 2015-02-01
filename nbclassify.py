0a00aa12e2bc18c1d588c1afed77b5e8aada92ee
Atmananda Persaud <apersaud@usc.edu>
2015-01-31 17:34:28 -0800

    updating nbclassify.py. some helper functions remaining

--- nbclassify.py
+++ nbclassify.py
@@ -1,6 +1,14 @@
 import sys
 import json
 
+def classify(dictionary, doc):
+    classprobs = [compute_classprob(dictionary, key, doc) for key in dictionary]
+    #need to get argmax
+
+def compute_classprob(dictionary, docclass, doc):
+    #compute prior
+    #compute conditional probability for each of the words in the doc
+
 def main():
     modelfile = open(sys.argv[1])
     testfile = open(sys.argv[2])
@@ -9,6 +17,10 @@ def main():
 
     num_doc_vec = [d[key]['NUM_DOCS'] for key in d]
     num_docs = sum(num_doc_vec)
+
+    for line in testfile:
+        pred = classify(wc_dict, line)
+        print(pred)
     
 if __name__ == '__main__':
     main()
